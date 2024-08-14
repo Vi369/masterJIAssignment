f4f3ad8cb8bc8e06027a2f8e2c08ee616541d065
Vikas singh varma <Vikashverma3001@gmail.com>
2024-08-14 15:17:03 +0530

    initial setup done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+        <h1 className='bg-orange-600'>Hello</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/Store/randomUserSlice.js
@@ -0,0 +1,8 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    
+}
+const randomUserSlice = createSlice({
+
+})
